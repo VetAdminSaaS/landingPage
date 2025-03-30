925ffec73a9c779b58e4b8a902d453f008db04e2
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-03-30 16:14:29 -0500

    Subiendo primer navbar a feature/diseño-inicial

--- src/app/shared/component/navbar/navbar.component.ts
+++ src/app/shared/component/navbar/navbar.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, HostListener, inject } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
+  isMenuOpen = false;
+  constructor(public router: Router) {}
+  
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    document.body.style.overflow = this.isMenuOpen ? 'hidden' : 'auto';
+  }
+
+  openLogin(): void {
+    console.log('Abrir modal de login');
+    if (this.isMenuOpen) this.toggleMenu();
+    // this.router.navigate(['/login']); // Opcional: navegar a ruta de login
+  }
+
+  openSignup(): void {
+    console.log('Abrir modal de registro');
+    if (this.isMenuOpen) this.toggleMenu();
+    // this.router.navigate(['/signup']); // Opcional: navegar a ruta de registro
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    if (event.target.innerWidth > 768 && this.isMenuOpen) {
+      this.toggleMenu();
+    }
+  }
 
 }
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  mode: 'jit',
+  content: [
+    "./src/**/*.{html,ts,scss}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
