30d6110b86abffd566903ec28c1ec2bb4b8e5cbb
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-04-02 11:59:42 -0500

    Desarrollo del diseño de feature/design/services-landing

--- src/app/pages/service-landing/service-landing.component.ts
+++ src/app/pages/service-landing/service-landing.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/component/navbar/navbar.component";
+import { FooterComponent } from "../../shared/component/footer/footer.component";
 
 @Component({
   selector: 'app-service-landing',
   standalone: true,
-  imports: [NavbarComponent],
+  imports: [NavbarComponent, FooterComponent],
   templateUrl: './service-landing.component.html',
   styleUrl: './service-landing.component.scss'
 })
