35bf134e1acd1a2e1d7f9172c55a688864809e09
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-04-02 14:38:17 -0500

    Desarrollo de diseño feature/benefits

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/component/navbar/navbar.component";
 import { AboutComponent } from "../../shared/component/about/about.component";
+import { BenefitsComponent } from "../../shared/component/benefits/benefits.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, AboutComponent],
+  imports: [NavbarComponent, AboutComponent, BenefitsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
