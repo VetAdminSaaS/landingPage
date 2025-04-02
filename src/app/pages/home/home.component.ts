f9bac2f762a3f5d2aa793871b283835efe94d572
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-04-02 15:23:05 -0500

    Desarrollo completo feature/landing-major

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,11 +3,13 @@ import { NavbarComponent } from "../../shared/component/navbar/navbar.component"
 import { AboutComponent } from "../../shared/component/about/about.component";
 import { BenefitsComponent } from "../../shared/component/benefits/benefits.component";
 import { ContactComponent } from "../../shared/component/contact/contact.component";
+import { TestimonialsComponent } from "../../shared/component/testimonials/testimonials.component";
+import { FooterComponent } from "../../shared/component/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [NavbarComponent, AboutComponent, BenefitsComponent, ContactComponent],
+  imports: [NavbarComponent, AboutComponent, BenefitsComponent, ContactComponent, TestimonialsComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
