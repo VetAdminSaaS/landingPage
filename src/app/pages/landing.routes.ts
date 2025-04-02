2589bafa5bfe25f7cf447dc17bc3304aaf1f80cf
Saavedra Sebastian <ssaavedraa4@upao.edu.pe>
2025-04-02 15:37:50 -0500

    Desarrollo de diseño para feature/landing-plans

--- src/app/pages/landing.routes.ts
+++ src/app/pages/landing.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from "@angular/router";
 import { HomeComponent } from "./home/home.component";
 import { ServiceLandingComponent } from "./service-landing/service-landing.component";
+import { PlanesComponent } from "./planes/planes.component";
 
 export const landingRoutes : Routes = [
  {
@@ -10,5 +11,9 @@ export const landingRoutes : Routes = [
  {
    path:'services',
    component: ServiceLandingComponent
+ },
+ {
+  path:'planes',
+  component: PlanesComponent
  }
 ]
--- src/app/pages/service-landing/service-landing.component.ts
+++ src/app/pages/service-landing/service-landing.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../shared/component/navbar/navbar.component";
 import { FooterComponent } from "../../shared/component/footer/footer.component";
+import { ContactComponent } from "../../shared/component/contact/contact.component";
 
 @Component({
   selector: 'app-service-landing',
   standalone: true,
-  imports: [NavbarComponent, FooterComponent],
+  imports: [NavbarComponent, FooterComponent, ContactComponent],
   templateUrl: './service-landing.component.html',
   styleUrl: './service-landing.component.scss'
 })
